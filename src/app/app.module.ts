68a1b16f31699fc169c0b69a27637680753aec07
coder-estnefo <ofentsemasita@gmail.com>
2020-09-17 12:23:51 +0200

    moved firebase configuration to environments and imported environments

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,16 +12,7 @@ import { HomeComponent } from './component/home/home.component';
 import { EmailLoginComponent } from './component/email-login/email-login.component';
 import { PhoneLoginComponent } from './component/phone-login/phone-login.component';
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDUtLIx6HuEXLY3008yUDZ6cUen59A6cO8",
-  authDomain: "mock-01.firebaseapp.com",
-  databaseURL: "https://mock-01.firebaseio.com",
-  projectId: "mock-01",
-  storageBucket: "mock-01.appspot.com",
-  messagingSenderId: "328761203115",
-  appId: "1:328761203115:web:e9a11c0a908c50cb57daee",
-  measurementId: "G-YN9P6KQWS8"
-};
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -36,7 +27,7 @@ const firebaseConfig = {
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireModule.initializeApp(environment.firebaseConfig),
     AngularFireAuthModule
   ],
   providers: [],
