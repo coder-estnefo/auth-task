fa593a66c9022d61e14086f649f8e4702136bdd6
coder-estnefo <ofentsemasita@gmail.com>
2021-01-21 18:50:44 +0200

    update

--- src/app/component/phone-login/phone-login.component.ts
+++ src/app/component/phone-login/phone-login.component.ts
@@ -7,26 +7,47 @@ import { environment } from '../../../environments/environment';
 @Component({
   selector: 'app-phone-login',
   templateUrl: './phone-login.component.html',
-  styleUrls: ['./phone-login.component.scss']
+  styleUrls: ['./phone-login.component.scss'],
 })
 export class PhoneLoginComponent implements OnInit {
-
   phone;
   code;
+  verifyDisabled = true;
+  countryCode = '+27';
 
-  constructor(private loginAuthService: LoginAuthService) { }
+  constructor(private loginAuthService: LoginAuthService) {}
 
   ngOnInit(): void {
-    firebase.initializeApp(environment.firebaseConfig);
+    if (firebase.apps.length == 0) {
+      firebase.initializeApp(environment.firebaseConfig);
+    }
     this.loginAuthService.recaptcha();
   }
 
   sendLoginCode() {
-    this.loginAuthService.sendCode(this.phone);
+    if (this.phone != undefined) {
+      if (this.loginAuthService.recaptchaOk) {
+        if (this.phone.toString().length != 9) {
+          alert('Only Enter the Nine Numbers After Zero!');
+        } else {
+          this.phone = this.phone.toString();
+          this.loginAuthService.sendCode(this.countryCode + this.phone);
+          this.verifyDisabled = false;
+        }
+      } else {
+        alert('Confirm You Are Not A Robot!');
+      }
+    } else {
+      alert('Enter the Nine Numbers After Zero!');
+    }
   }
 
   verifyLoginCode() {
-    this.loginAuthService.verifyCode(this.code);
+    if (this.code != undefined && this.code.toString().length == 6) {
+      this.code = this.code.toString();
+      this.loginAuthService.verifyCode(this.code);
+    } else {
+      alert('Enter A Valid Code!');
+    }
   }
-
 }
--- src/app/service/login-auth.service.ts
+++ src/app/service/login-auth.service.ts
@@ -4,86 +4,128 @@ import { Router } from '@angular/router';
 import { auth } from 'firebase/app';
 import { WindowService } from './window.service';
 
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LoginAuthService {
-
   windowRef;
+  recaptchaOk = false;
 
   constructor(
     private auth: AngularFireAuth,
     private router: Router,
     private win: WindowService
-  ) { }
+  ) {}
 
   createUser(email, password) {
-    this.auth.createUserWithEmailAndPassword(email, password).then( () => {
-      console.log('account created');
-    }).catch((error) => {
-      console.log(error.message);
-    });
+    this.auth
+      .createUserWithEmailAndPassword(email, password)
+      .then(() => {
+        console.log('account created');
+      })
+      .catch((error) => {
+        console.log(error.message);
+        alert('Email Already Exists');
+      });
   }
 
   loginEmailPassword(email, password) {
-    this.auth.signInWithEmailAndPassword(email, password).then( () => {
-      console.log('email and password successful login');
-      this.router.navigateByUrl('/home');
-    }).catch( (error) => {
-      console.log(error.message);
-    });
+    this.auth
+      .signInWithEmailAndPassword(email, password)
+      .then((user) => {
+        console.log('email and password successful login');
+        this.router.navigateByUrl('/home');
+      })
+      .catch((error) => {
+        console.log(error.message);
+        alert('Incorrect Login Details');
+      });
   }
 
   googleSignIn() {
-    this.auth.signInWithPopup(new auth.GoogleAuthProvider()).then(() => {
-      console.log('google login successful');
-      this.router.navigateByUrl('/home');
-    }).catch((error) => {
-      console.log(error.message);
-    });
+    this.auth
+      .signInWithPopup(new auth.GoogleAuthProvider())
+      .then(() => {
+        console.log('google login successful');
+        this.router.navigateByUrl('/home');
+      })
+      .catch((error) => {
+        console.log(error.message);
+        alert(error.message);
+      });
   }
 
   facebookSignIn() {
-    this.auth.signInWithPopup(new auth.FacebookAuthProvider()).then(() => {
-      console.log('facebook successful login');
-      this.router.navigateByUrl('/home');
-    }).catch((error) => {
-      console.log(error.message);
-    });
+    this.auth
+      .signInWithPopup(new auth.FacebookAuthProvider())
+      .then(() => {
+        console.log('facebook successful login');
+        this.router.navigateByUrl('/home');
+      })
+      .catch((error) => {
+        console.log(error.message);
+        alert(error.message);
+      });
   }
 
   phoneSignIn() {
-    this.auth.signInWithPopup(new auth.PhoneAuthProvider()).then(() => {
-      console.log('phone successful login');
-      this.router.navigateByUrl('/home');
-    }).catch((error) => {
-      console.log(error.message);
-    });
+    this.auth
+      .signInWithPopup(new auth.PhoneAuthProvider())
+      .then(() => {
+        console.log('phone successful login');
+        this.router.navigateByUrl('/home');
+      })
+      .catch((error) => {
+        console.log(error.message);
+        alert(error.message);
+      });
   }
 
   recaptcha() {
     this.windowRef = this.win.windowRef;
-    this.windowRef.recaptchaVerifier = new auth.RecaptchaVerifier('recaptcha-container');
-    this.windowRef.recaptchaVerifier.render();
+    this.windowRef.recaptchaVerifier = new auth.RecaptchaVerifier(
+      'recaptcha-container',
+      {
+        size: 'normal',
+        callback: (response) => {
+          // reCAPTCHA solved, allow signInWithPhoneNumber.
+          this.recaptchaOk = true;
+        },
+        'expired-callback': () => {
+          // Response expired. Ask user to solve reCAPTCHA again.
+          // ...
+        },
+      }
+    );
+    this.windowRef.recaptchaVerifier.render().then((widgetID) => {
+      this.windowRef.recaptchaWidgetId = widgetID;
+    });
   }
 
   sendCode(phone) {
     const appVerifier = this.windowRef.recaptchaVerifier;
-    this.auth.signInWithPhoneNumber(phone, appVerifier).then((result) => {
-      this.windowRef.confirmationResult = result;
-      console.log('sending code');
-    }).catch((error) => {
-      console.log(error.message);
-    });
+    this.auth
+      .signInWithPhoneNumber(phone, appVerifier)
+      .then((result) => {
+        this.windowRef.confirmationResult = result;
+        console.log('sending code');
+      })
+      .catch((error) => {
+        console.log(error.message);
+      });
   }
 
   verifyCode(code) {
-    this.windowRef.confirmationResult.confirm(code).then(() => {
-      console.log('code verified');
-    }).catch((error) =>{
-      console.log(error.message);
-    });
+    this.windowRef.confirmationResult
+      .confirm(code)
+      .then(() => {
+        console.log('code verified');
+        this.router.navigateByUrl('/home');
+      })
+      .catch((error) => {
+        console.log(error.message);
+        alert(error.message);
+      });
   }
 
   logout() {
